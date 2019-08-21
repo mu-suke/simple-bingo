f57a556455404827e1c91f2221647b740c830033
mu-suke <45083522+mu-suke@users.noreply.github.com>
2019-08-21 18:32:50 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app" v-cloak>
+    <Bingo></Bingo>
+  </div>
+</template>
+
+<script>
+import Bingo from './components/Bingo'
+
+export default {
+  name: 'app',
+  components: {
+    Bingo
+  }
+}
+</script>
+
+<style lang="scss">
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+</style>
+
+// kebab-case
